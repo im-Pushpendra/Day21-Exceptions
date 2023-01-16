c8a44a4751bad3120d8997a171f81efa4a37726c
im-Pushpendra <112095599+im-Pushpendra@users.noreply.github.com>
2023-01-16 22:11:39 +0530

    Addded: Mood Informed to User

---
+++ MoodAnalyser.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.day21.UC_Day21_Exceptions;
+
+public class MoodAnalyser {
+
+	public static void main(String[] args) {
+		System.out.println("Handle Custom\r\n"
+				+ "Exceptions");
+
+	}
+
+}
